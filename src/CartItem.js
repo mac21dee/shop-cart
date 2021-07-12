f201c6acac9a1dbcb01aa5252f67c2fbede28150
mac21dee <Deepakkr21092000@gmail.com>
2021-07-12 12:08:09 +0530

    second commit

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            price : 9999,
+            title : 'Mobile Phone',
+            qty : 1,
+            img : ''
+        }
+    }
+
+    increaseQuantity = () => {
+        console.log('this.state',this.state)
+    }
     render(){
+        const {price,title,qty}  = this.state ;
         return (
             <div className = "cart-item">
                 <div className = "left-block">
@@ -9,11 +23,32 @@ class CartItem extends React.Component {
                 </div>
 
                 <div className = "right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs. 9,999</div>
-                    <div style={{color:'#777'}}>Oty: 1</div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs. {price}</div>
+                    <div style={{color:'#777'}}>Oty: {qty}</div>
                     <div className = "cart-item-actions">
                         {/*Buttons*/}
+                        <img 
+                            alt = "increase" 
+                            className = "action-icons" 
+                            src = "https://image.flaticon.com/icons/png/128/1828/1828926.png"
+                            onClick = {this.increaseQuantity}>
+
+                        </img>
+                        <img 
+                            alt = "decrease" 
+                            className = "action-icons" 
+                            src = "https://image.flaticon.com/icons/png/128/992/992683.png">
+
+                        </img>
+                        <img 
+                            alt = "delete" 
+                            className = "action-icons" 
+                            src = "https://image.flaticon.com/icons/png/128/1214/1214428.png"
+                            
+                            >
+                            
+                        </img>
                     </div>
                 </div>
             </div>
